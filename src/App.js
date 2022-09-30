21688b88c94d3831c3a704ed33fa16d48834a548
t-kalden <kalden.t@outlook.com>
2022-09-30 10:49:23 -0400

    Implementing React Router to add About page and doing minor styling debuging

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { v4 as uuidv4 } from 'uuid'
 import {useState} from 'react'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+
 import Header from './components/Header'
 import FeedbackData from './data/FeedBackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 export default function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -23,20 +27,30 @@ export default function App() {
     }
 
     return (
-        <>
-            <Header/>
+        <Router>
+            <Header />
             <div className="container">
-                <FeedbackForm handleAdd = {addFeedback}/>
-                <FeedbackStats 
-                    feedback={feedback}
-                />
-                <FeedbackList 
-                    feedback={feedback}
-                    handleDelete = {deleteFeedback}
-                />
-            </div>
-        </>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd = {addFeedback}/>
+                            <FeedbackStats 
+                                feedback={feedback}
+                            />
+                            <FeedbackList 
+                                feedback={feedback}
+                                handleDelete = {deleteFeedback}
+                            />
+                        </>
+                    }>
 
+                    </Route>
+                    <Route path='/about' element={<AboutPage/>}/>
+                </Routes>
+                <AboutIconLink/>
+                {console.log(Route.name)}
+            </div>
+        </Router>
     )
 }
 
